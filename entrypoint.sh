81722018b95d67874dc1ab7589f6d6ce531e153e
nbouaziz-decat-hk <53844161+nbouaziz-decat-hk@users.noreply.github.com>
2020-12-09 12:50:42 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,8 +10,8 @@ SSH_PATH="$HOME/.ssh"
 mkdir -p "$SSH_PATH"
 
 # Place deploy_key into .ssh dir
-echo "${INPUT_SSH_PRIVATE_KEY}" > "$SSH_PATH/deploy_key"
-chmod 600 "$SSH_PATH/deploy_key"
+echo "${INPUT_SSH_PRIVATE_KEY}" > "$SSH_PATH/ssh_key"
+chmod 600 "$SSH_PATH/ssh_key"
 
 # rsync the data
-sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' ${INPUT_SOURCE_FOLDER} ${INPUT_SSH_REMOTE_USER}@${INPUT_SSH_REMOTE_HOST}:${INPUT_TARGET_FOLDER}"
+sh -c "rsync ${INPUT_RSYNC_OPTIONS} -e 'ssh -i $SSH_PATH/ssh_key -o StrictHostKeyChecking=no' ${INPUT_SOURCE_FOLDER} ${INPUT_SSH_REMOTE_USER}@${INPUT_SSH_REMOTE_HOST}:${INPUT_TARGET_FOLDER}"
